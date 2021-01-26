1bc9dd13a1b7c5fd82c1db9d9a6096b32d442e7a
Pranav <77088825+pranavjha21@users.noreply.github.com>
2021-01-26 16:39:48 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -506,7 +506,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I am now offline!")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "good night chlo mai chala sone (mai chota sa naha sa baccha ho) matab kiido !")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!")
